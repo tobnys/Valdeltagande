7282e2c845b75743df19a0998b61de86ad9183e0
tobnys <tobnystrom@gmail.com>
2017-12-02 01:38:47 +0100

    Restructuring code

--- public/client.js
+++ public/client.js
@@ -4,6 +4,7 @@ let settings = {
   method: "GET",
 }
 
+/*
 // Enkelt GET request till API:et i fråga, tar emot datan och skickar det vidare för utskrift.
 fetch(apiUrl, settings).then(function(res) {
   if (res.ok) {
@@ -21,7 +22,19 @@ fetch(apiUrl, settings).then(function(res) {
       throw new Error(e);
     }
 });
+*/
 
+callInternalAPI(1973)
+
+// Samordnar alla funktioner och printar ut det slutgiltiga.
+function tick(data, region, value){
+  for(let i = 1; i<data.variables[2].values.length; i++){
+    callInternalAPI(data.variables[2].values[i]);
+    console.log(`Valår: ${data.variables[2].values[i]} | Region: ${region} | Procent: ${value}`);
+  }
+}
+
+// Anropa vår egna API för att göra ett POST request till SCB's API.
 function callInternalAPI(year){
   fetch(`/api/${year}`, settings).then(function(res) {
     if (res.ok) {
@@ -50,23 +63,22 @@ function findMaxValue(data){
       largestValueCode = data.data[i].key[0];
     }
   }
-  findRegion(largestValueCode);
+  findRegion(largestValueCode, largestValue);
 }
 
 // Hitta regionen baserat på nummret vi fick i den tidigare funktionen ovan.
-function findRegion(code){
+function findRegion(code, value){
   let region = null;
 
   fetch(apiUrl, settings).then(function(res) {
     if (res.ok) {
       res.json().then(function(data) {
-        console.log(data.variables[0]);
         for(let i = 0; i < data.variables[0].values.length; i++){
           if(data.variables[0].values[i] === code){
             region = data.variables[0].valueTexts[i];
           }
         }
-        logResults(data, region, value);
+        tick(data, region, value);
       });
     } else {
       console.log("Response was not OK, instead got: ", res.status);
@@ -76,22 +88,11 @@ function findRegion(code){
         throw new Error(e);
       }
   });
-
-  finalRegion = region;
 }
 
-// Denna funktion hanterar utskrift av information.
-function logResults(data, region, value){
-  console.log(data.variables[0]);
-
-  // Vi baserar produktionen på hur många valår det är, en enkel loop sköter detta.
-  for(let i = 0; i<data.variables[2].values.length; i++){
-    callInternalAPI(data.variables[2].values[i]);
-    console.log(`Valår: ${data.variables[2].values[i]} | Region: ${finalRegion} | Procent: ${finalPercent}`);
-  }
-}
 
 /* FÖRBÄTTRINGAR:
 1. Hämta endast relevant information ifrån API:et istället för att hämta hem allt. På en liten skala gör detta inte något men ska det skalas upp kan det bli ett problem.
 2. Ändra lite säkerhet så att om två regioner har likadana valdeltaganden så får de en delad plats.
+3. Väldigt dåligt upplägg på koden, inte speciellt genomtänkt då det var väldigt knepigt att få API:n att funka.
 */
